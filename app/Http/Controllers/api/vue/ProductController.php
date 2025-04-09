435a1ba4b3202eb1aaa072c11e07e8ca817734c9
razib-cloud <razibidb61@gmail.com>
2025-04-09 13:06:27 +0600

    product api

--- app/Http/Controllers/api/vue/ProductController.php
+++ app/Http/Controllers/api/vue/ProductController.php
@@ -9,32 +9,78 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
 
-    public function index()
-    {
-        try {
-            $products = Product::all();
-            if (!$products) {
-                $products = "Data Not Found";
-            }
-            return response()->json(["products" => Product::all()]);
-        } catch (\Throwable $th) {
+    // public function index()
+    // {
+    //     try {
+    //         $products = Product::all();
+    //         if (!$products) {
+    //             $products = "Data Not Found";
+    //         }
+    //         return response()->json(["products" => Product::all()]);
+    //     } catch (\Throwable $th) {
 
-            return response()->json(["error" => $th->getMessage()]);
+    //         return response()->json(["error" => $th->getMessage()]);
+    //     }
+    // }
+
+
+    public function index(Request $request)
+    {
+        $query = Product::query();
+
+        if ($request->search) {
+            $query->where('name', 'like', "%{$request->search}%");
         }
+        return response()->json($query->paginate(5));
     }
 
 
     public function store(Request $request)
     {
-        //
+
+        try {
+            $product = new Product();
+            $product->name=$request->name;
+            $product->menu_id=$request->menu_id;
+            $product->price=$request->price;
+            $product->photo=$request->photo;
+            $product->description=$request->description;
+
+
+
+
+            date_default_timezone_set("Asia/Dhaka");
+            $product->created_at=date('Y-m-d H:i:s');
+            $product->updated_at=date('Y-m-d H:i:s');
+
+            if(isset($request->photo)){
+                $product->photo=$request->photo;
+            }
+
+            $product->save();
+            if(isset($request->photo)){
+                $imageName=$product->id.'.'.$request->photo->extension();
+                $product->photo=$imageName;
+                $product->update();
+                $request->photo->move(public_path('products'),$imageName);
+            }
+            return response()->json(["roles"=>  $product ]);
+        } catch (\Throwable $th) {
+           return response()->json(["error"=> $th->getMessage()]);
+        }
+
     }
 
 
     public function show($id)
     {
-        //
+        try {
+            $product =  Product::find($id);
+            return response()->json(["products" =>  $product]);
+        } catch (\Throwable $th) {
+            return response()->json(["error" => $th->getMessage()]);
+        }
     }
-
     public function update(Request $request, $id)
     {
         //
